8063c5c39d16c76d820a4a7cebf3b7d5bbddfc27
Даниил <madcatteam@gmail.com>
2025-01-09 14:11:28 +0300

    Добавил разделение на позитивные и негативные тесты

--- 04_lesson/test_stringutils.py
+++ 04_lesson/test_stringutils.py
@@ -8,16 +8,28 @@ def utils():
 
 
 def test_capitilize(utils):
+
+    # Positive
+
     assert utils.capitilize("skypro") == "Skypro"
+
+    # Negative
+
     assert utils.capitilize("") == ""
     assert utils.capitilize("hELLO") == "Hello"
 
 
 @pytest.mark.parametrize("input_string, expected_output", [
+
+    # Positive
+
     ("   skypro", "skypro"),
     ("skypro   ", "skypro   "),
-    ("   ", ""),
     ("test", "test"),
+
+    # Negative
+
+    ("   ", ""),
     ("", "")
 ])
 def test_trim(utils, input_string, expected_output):
@@ -25,17 +37,29 @@ def test_trim(utils, input_string, expected_output):
 
 
 @pytest.mark.parametrize("input_string, delimeter, expected_output", [
+
+    # Positive
+
     ("a,b,c,d", ",", ["a", "b", "c", "d"]),
     ("1:2:3", ":", ["1", "2", "3"]),
-    ("", ",", []),
-    ("a b c", " ", ["a", "b", "c"])
+    ("a b c", " ", ["a", "b", "c"]),
+
+    # Negative
+
+    ("", ",", [])
 ])
 def test_to_list(utils, input_string, delimeter, expected_output):
     assert utils.to_list(input_string, delimeter) == expected_output
 
 
 @pytest.mark.parametrize("input_string, symbol, expected_output", [
+
+    # Positive
+
     ("SkyPro", "S", True),
+
+    # Negative
+
     ("SkyPro", "U", False),
     ("", "a", False)
 ])
@@ -44,8 +68,14 @@ def test_contains(utils, input_string, symbol, expected_output):
 
 
 @pytest.mark.parametrize("input_string, symbol, expected_output", [
+
+    # Positive
+
     ("SkyPro", "k", "SyPro"),
     ("SkyPro", "Pro", "Sky"),
+
+    # Negative
+
     ("SkyPro", "x", "SkyPro")
 ])
 def test_delete_symbol(utils, input_string, symbol, expected_output):
@@ -53,7 +83,13 @@ def test_delete_symbol(utils, input_string, symbol, expected_output):
 
 
 @pytest.mark.parametrize("input_string, symbol, expected_output", [
+
+    # Positive
+
     ("SkyPro", "S", True),
+
+    # Negative
+
     ("SkyPro", "P", False),
     ("", "S", False)
 ])
@@ -62,7 +98,13 @@ def test_starts_with(utils, input_string, symbol, expected_output):
 
 
 @pytest.mark.parametrize("input_string, symbol, expected_output", [
+
+    # Positive
+
     ("SkyPro", "o", True),
+
+    # Negative
+
     ("SkyPro", "y", False),
     ("", "o", False)
 ])
@@ -71,8 +113,14 @@ def test_end_with(utils, input_string, symbol, expected_output):
 
 
 @pytest.mark.parametrize("input_string, expected_output", [
+
+    # Positive
+
     ("", True),
     ("   ", True),
+
+    # Negative
+
     ("SkyPro", False)
 ])
 def test_is_empty(utils, input_string, expected_output):
@@ -80,9 +128,15 @@ def test_is_empty(utils, input_string, expected_output):
 
 
 @pytest.mark.parametrize("lst, joiner, expected_output", [
+
+    # Positive
+
     ([1, 2, 3, 4], ", ", "1, 2, 3, 4"),
     (["Sky", "Pro"], ", ", "Sky, Pro"),
     (["Sky", "Pro"], "-", "Sky-Pro"),
+
+    # Negative
+
     ([], ", ", "")
 ])
 def test_list_to_string(utils, lst, joiner, expected_output):
